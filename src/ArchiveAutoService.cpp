de1f19b5bdf4f38671904d1f3e8190d90647df21
Jeremy Rimpo <jrim@rimpo.org>
2023-10-11 20:35:28 -0500

    Move try/catch to thread and return result

--- src/ArchiveAutoService.cpp
+++ src/ArchiveAutoService.cpp
@@ -7,28 +7,28 @@ namespace BsaPacker
 {
 	bool ArchiveAutoService::CreateBSA(libbsarch::bs_archive_auto* archive, const QString& archiveName, const bsa_archive_type_e type) const
 	{
-		try {
-			QProgressDialog savingDialog;
-			savingDialog.setWindowFlags(savingDialog.windowFlags() & ~Qt::WindowCloseButtonHint);
-			savingDialog.setWindowTitle(QObject::tr("Writing Archive"));
-			savingDialog.setCancelButton(0);
-			QLabel text;
-			text.setText(QObject::tr("Writing %1").arg(archiveName));
-			savingDialog.setLabel(&text);
-			savingDialog.setRange(0, 0);
-			savingDialog.show();
-			auto future = QtConcurrent::run([this, archive, archiveName]() {
+		QProgressDialog savingDialog;
+		savingDialog.setWindowFlags(savingDialog.windowFlags() & ~Qt::WindowCloseButtonHint);
+		savingDialog.setWindowTitle(QObject::tr("Writing Archive"));
+		savingDialog.setCancelButton(0);
+		QLabel text;
+		text.setText(QObject::tr("Writing %1").arg(archiveName));
+		savingDialog.setLabel(&text);
+		savingDialog.setRange(0, 0);
+		savingDialog.show();
+		auto future = QtConcurrent::run([=]() -> bool {
+			try {
 				archive->save_to_disk(archiveName.toStdString());
-				});
-			while (!future.isFinished())
-			{
-				QCoreApplication::processEvents();
+			} catch (std::exception e) {
+				return false;
 			}
-			savingDialog.hide();
+			return true;
+			});
+		while (!future.isFinished())
+		{
+			QCoreApplication::processEvents();
 		}
-		catch (std::exception e) {
-			return false;
-		}
-		return true;
+		savingDialog.hide();
+		return future.result();
 	}
 } // namespace BsaPacker
