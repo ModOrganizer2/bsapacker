da1ef718560eece42776564571a41b937dfce9f5
Liderate <Liderate@users.noreply.github.com>
2024-02-06 18:43:36 -0500

    Don't append number to first archive to avoid changing dummy plugin code

--- src/ArchiveNameService.cpp
+++ src/ArchiveNameService.cpp
@@ -29,7 +29,10 @@ namespace BsaPacker
 
 	QString ArchiveNameService::GetArchiveFullPath(const bsa_archive_type_e type, const IModDto* modDto, const int archiveIndex) const
 	{
-		return QDir::toNativeSeparators(modDto->Directory() + '/' + modDto->ArchiveName() + this->Infix(type) + QString::number(archiveIndex + 1) + this->GetFileExtension());
+		if (archiveIndex == 0) {
+			return QDir::toNativeSeparators(modDto->Directory() + '/' + modDto->ArchiveName() + this->Infix(type) + this->GetFileExtension());
+		}
+		return QDir::toNativeSeparators(modDto->Directory() + '/' + modDto->ArchiveName() + this->Infix(type) + QString::number(archiveIndex) + this->GetFileExtension());
 	}
 
 	QString ArchiveNameService::Infix(const bsa_archive_type_e type) const
