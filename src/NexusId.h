246aa73ac7ddbd8fd659c1b7142eb852087b1c6f
Liderate <Liderate@users.noreply.github.com>
2024-03-06 14:06:22 -0500

    Support TTW, Enderal, and EnderalSE

--- src/NexusId.h
+++ src/NexusId.h
@@ -5,6 +5,7 @@ namespace BsaPacker
 {
 	enum NexusId
 	{
+		// Skyrim VR, Fallout 4 VR, and TTW don't have Nexus pages so are 0
 		Morrowind = 100,
 		Oblivion = 101,
 		Fallout3 = 120,
@@ -12,6 +13,8 @@ namespace BsaPacker
 		Skyrim = 110,
 		SkyrimSE = 1704,
 		Fallout4 = 1151,
+		Enderal = 2736,
+		EnderalSE = 3685,
 		Starfield = 4187
 	};
 } // namespace BsaPacker
