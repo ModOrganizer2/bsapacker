e1ada32175ac90e354f298d534d4df3812825867
Liderate <Liderate@users.noreply.github.com>
2024-03-11 01:12:13 -0400

    Split archives

--- src/BsaPackerWorker.cpp
+++ src/BsaPackerWorker.cpp
@@ -29,20 +29,29 @@ namespace BsaPacker
 
 	void BsaPackerWorker::DoWork() const
 	{
+		QStringList createdArchives;
 		const std::unique_ptr<IModDto> modDto = this->m_ModDtoFactory->Create(); // handles PackerDialog and validation, implements Null Object pattern
 		const std::vector<bsa_archive_type_e> types = this->m_ArchiveBuilderFactory->GetArchiveTypes(modDto.get());
 		for (auto&& type : types) {
 			const std::unique_ptr<IArchiveBuilder> builder = this->m_ArchiveBuilderFactory->Create(type, modDto.get());
 			ArchiveBuildDirector director(this->m_SettingsService, builder.get());
 			director.Construct(); // must check if cancelled
-			const std::unique_ptr<libbsarch::bs_archive_auto> archive = builder->getArchive();
-			if (archive) {
-				const QString& archiveFullPath = this->m_ArchiveNameService->GetArchiveFullPath(type, modDto.get());
-				bool res = this->m_ArchiveAutoService->CreateBSA(archive.get(), archiveFullPath, type);
-				if (res)
-					QMessageBox::information(nullptr, "", QObject::tr("Created") + " \"" + archiveFullPath + "\"");
+			const std::vector<std::unique_ptr<libbsarch::bs_archive_auto>> archives = builder->getArchives();
+			for (const auto& archive : archives) {
+				if (archive) {
+					const QString& archiveFullPath = this->m_ArchiveNameService->GetArchiveFullPath(type, modDto.get());
+					bool res = this->m_ArchiveAutoService->CreateBSA(archive.get(), archiveFullPath, type);
+					if (res) {
+						createdArchives.append(QFileInfo(archiveFullPath).fileName());
+					}
+				}
 			}
 		}
+
+		if (!createdArchives.isEmpty()) {
+			QMessageBox::information(nullptr, "", QObject::tr("Created archive(s):") + "\n" + createdArchives.join(",\n"));
+		}
+
 		const std::unique_ptr<IDummyPluginService> pluginService = this->m_DummyPluginServiceFactory->Create();
 		pluginService->CreatePlugin(modDto->Directory(), modDto->ArchiveName());
 
